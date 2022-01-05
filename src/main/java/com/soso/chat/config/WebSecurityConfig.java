590f95a61c132f3d4c054d2cb79a0602481820ac
sosoKim9966 <sosokim9966@gmail.com>
2022-01-05 23:50:03 +0900

    20210105_security&jwt

---
+++ src/main/java/com/soso/chat/config/WebSecurityConfig.java
@@ -0,0 +1,6 @@
+package com.soso.chat.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
