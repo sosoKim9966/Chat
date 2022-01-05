cfa745c5c4b4650a1900aee5ae31e9bfccca1bf3
sosoKim9966 <sosokim9966@gmail.com>
2022-01-06 01:20:11 +0900

    20210105_security&jwt2

--- src/main/java/com/soso/chat/config/WebSecurityConfig.java
+++ src/main/java/com/soso/chat/config/WebSecurityConfig.java
@@ -1,6 +1,26 @@
 package com.soso.chat.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .headers()
+                .frameOptions().sameOrigin()// SockJS는 기본적으로 HTML iframe 요소를 통한 전송을 허용하지 않도록 설정되는데 해당 내용을 해제한다.
+            .and()
+                .formLogin()
+            .and()
+                .authorizeRequests()
+                .antMatchers("/chat/**").hasRole("USER")
+                .anyRequest().permitAll();
+    }
+
 }
